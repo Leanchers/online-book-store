51da737065e058d65a4fdbd31d56cf9757bbdb07
AlexL <alexlada86@gmail.com>
2024-06-19 00:15:24 +0200

    git add bookrepository

---
+++ src/main/java/mate/academy/onlinebookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package mate.academy.onlinebookstore.repository;
+
+import java.util.List;
+import mate.academy.bookstore.model.Book;
+
+public interface BookRepository {
+    Book save(Book book);
+    List findAll();
+}
