5b84ad6a44564d674c0e81b12f12cedba8fa4749
AlexL <alexlada86@gmail.com>
2024-06-20 09:18:16 +0200

    fixed misstakes 2

--- src/main/java/mate/academy/onlinebookstore/model/Book.java
+++ src/main/java/mate/academy/onlinebookstore/model/Book.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,5 +28,4 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
-
 }
