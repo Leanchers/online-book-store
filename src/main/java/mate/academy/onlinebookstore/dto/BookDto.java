d68bde2f6f71940423ed6b9891756d52fbb7ddcc
AlexL <alexlada86@gmail.com>
2024-06-21 12:12:08 +0200

    add dto

---
+++ src/main/java/mate/academy/onlinebookstore/dto/BookDto.java
@@ -0,0 +1,18 @@
+package mate.academy.onlinebookstore.dto;
+
+import jakarta.persistence.Column;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
