a313c1334fe62b658464992cab53c5fed28dc586
AlexL <alexlada86@gmail.com>
2024-07-18 20:58:30 +0200

    add userdetails and userdetailsservice

--- src/main/java/mate/academy/onlinebookstore/repository/user/UserRepository.java
+++ src/main/java/mate/academy/onlinebookstore/repository/user/UserRepository.java
@@ -1,8 +1,10 @@
 package mate.academy.onlinebookstore.repository.user;
 
+import java.util.Optional;
 import mate.academy.onlinebookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
