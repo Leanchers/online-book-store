9f9e2637931c1df615cc87842a953cbc44413070
AlexL <alexlada86@gmail.com>
2024-07-22 06:40:18 +0200

    add login method

--- src/main/java/mate/academy/onlinebookstore/controller/AuthController.java
+++ src/main/java/mate/academy/onlinebookstore/controller/AuthController.java
@@ -4,10 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mate.academy.onlinebookstore.dto.user.UserLoginRequestDto;
+import mate.academy.onlinebookstore.dto.user.UserLoginResponseDto;
 import mate.academy.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import mate.academy.onlinebookstore.dto.user.UserResponseDto;
 import mate.academy.onlinebookstore.exception.RegistrationException;
+import mate.academy.onlinebookstore.security.AuthenticationService;
 import mate.academy.onlinebookstore.service.user.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @Operation(summary = "Register a new user", description = "Register a new user")
     @PostMapping("/register")
     public UserResponseDto registerUser(@RequestBody @Valid UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         return userService.register(requestDto);
+    }
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
--- src/main/java/mate/academy/onlinebookstore/dto/user/UserLoginResponseDto.java
+++ src/main/java/mate/academy/onlinebookstore/dto/user/UserLoginResponseDto.java
@@ -1,2 +1,5 @@
-package mate.academy.onlinebookstore.dto.user;public record UserLoginResponseDto() {
+package mate.academy.onlinebookstore.dto.user;
+
+public record UserLoginResponseDto(String token) {
+
 }
