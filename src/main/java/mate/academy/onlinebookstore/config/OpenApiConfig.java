3f1d05e8078bf1110e738f01a6a430e398c6e101
AlexL <alexlada86@gmail.com>
2024-07-23 08:25:43 +0200

    refactored code

--- src/main/java/mate/academy/onlinebookstore/config/OpenApiConfig.java
+++ src/main/java/mate/academy/onlinebookstore/config/OpenApiConfig.java
@@ -5,17 +5,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OpenApiConfig {
-
+    private static String BEARER_AUTH = "BearerAuth";
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI()
-            .components(new Components().addSecuritySchemes("BearerAuth",
+            .components(new Components().addSecuritySchemes(BEARER_AUTH,
                 new SecurityScheme()
                     .type(SecurityScheme.Type.HTTP)
                     .scheme("bearer")
                     .bearerFormat("JWT")))
-            .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
+            .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
     }
 }
