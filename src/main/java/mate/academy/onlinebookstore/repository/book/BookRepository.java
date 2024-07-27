2723b665010ba18874ff754e2f20048c2866e8ab
AlexL <alexlada86@gmail.com>
2024-07-27 13:48:39 +0200

    add category model and repository

--- src/main/java/mate/academy/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/mate/academy/onlinebookstore/repository/book/BookRepository.java
@@ -1,8 +1,12 @@
 package mate.academy.onlinebookstore.repository.book;
 
+import java.util.List;
 import mate.academy.onlinebookstore.model.Book;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @EntityGraph(attributePaths = "categories")
+    List<Book> findAllByCategoryId(Long categoryId);
 }
