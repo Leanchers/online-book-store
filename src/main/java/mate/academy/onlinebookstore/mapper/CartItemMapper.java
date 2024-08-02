066602edebb678774bf5a760c74364a00833f2f2
AlexL <alexlada86@gmail.com>
2024-08-02 20:20:15 +0200

    add cart mappers

--- src/main/java/mate/academy/onlinebookstore/mapper/CartItemMapper.java
+++ src/main/java/mate/academy/onlinebookstore/mapper/CartItemMapper.java
@@ -1,2 +1,17 @@
-package mate.academy.onlinebookstore.mapper;public interface CartItemMapper {
+package mate.academy.onlinebookstore.mapper;
+
+import mate.academy.onlinebookstore.config.MapperConfig;
+import mate.academy.onlinebookstore.dto.shoppingcart.CartItemDto;
+import mate.academy.onlinebookstore.dto.shoppingcart.CreateCartItemRequestDto;
+import mate.academy.onlinebookstore.dto.shoppingcart.UpdateCartItemRequestDto;
+import mate.academy.onlinebookstore.model.CartItem;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface CartItemMapper {
+    CartItemDto toDto(CartItem cartItem);
+
+    CartItem toModel(CreateCartItemRequestDto cartItemDto);
+
+    CartItem toModel(UpdateCartItemRequestDto cartItemDto);
 }
--- src/main/java/mate/academy/onlinebookstore/mapper/ShoppingCartMapper.java
+++ src/main/java/mate/academy/onlinebookstore/mapper/ShoppingCartMapper.java
@@ -1,2 +1,11 @@
-package mate.academy.onlinebookstore.mapper;public interface ShoppingCartMapper {
+package mate.academy.onlinebookstore.mapper;
+
+import mate.academy.onlinebookstore.config.MapperConfig;
+import mate.academy.onlinebookstore.dto.shoppingcart.ShoppingCartDto;
+import mate.academy.onlinebookstore.model.ShoppingCart;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface ShoppingCartMapper {
+    ShoppingCartDto toDto(ShoppingCart shoppingCart);
 }
